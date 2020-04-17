0e5e455d8acdf00acf9268cfa08ab5d222d0b133
Dmitry Denisov <36160558+DmitriiDenisov@users.noreply.github.com>
2020-04-17 20:00:15 +0400

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -23,6 +23,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # VIM
+
+# Install VIM
+sudo apt-get update
+sudo apt-get install vim
+
 # 6. Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
