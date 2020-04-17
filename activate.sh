1bce908e1cc41049f8818a2e6c101d45a746c443
Dmitry Denisov <36160558+DmitriiDenisov@users.noreply.github.com>
2020-04-17 18:51:56 +0400

    Update activate.sh

--- activate.sh
+++ activate.sh
@@ -3,21 +3,40 @@ folder=$(dirname $filepath)
 binary=$(basename $filepath)
 working_dir=`pwd`
 
-# install zsh
+# 1. Install git
+sudo apt install git
+
+# 2. Install ZSH
+sudo apt install zsh
+
+# 3. Install Oh-My-ZSH
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # you need to exit after installation from child shell created
 
 # install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Copy files
-cp .vimrc ~/.vimrc
-cp .zshrc ~/.zshrc
+# 4. Install ZSH-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-# initialize Vim plugins
+# 5. Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# VIM
+# 6. Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+# 8. Install Plugins
 vim +PluginInstall +qall
 
 
+# Copy files
+cp -rf .vimrc ~/.vimrc
+cp -rf .zshrc ~/.zshrc
+
+# initialize Vim plugins
+vim +PluginInstall +qall
 
 cd ~
 echo "setting up personal .zshrc ..."
