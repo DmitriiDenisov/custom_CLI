b0812682950b1a9730e7841981c0a04b01c3ee90
Dmitry Denisov <36160558+DmitriiDenisov@users.noreply.github.com>
2020-04-17 23:21:59 +0400

    Update activate_mac.sh

--- activate_mac.sh
+++ activate_mac.sh
@@ -10,9 +10,38 @@ working_dir=`pwd`
 brew install zsh
 
 # take path to zsh
-which zsh
-chsh -s /usr/local/bin/zsh
+chsh -s /bin/zsh
 
 # Install Oh My ZSH:
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-chsh -s $(which zsh)
+
+# 4. Install ZSH-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# 5. Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# 6. Install vim
+brew install vim
+
+# 7. Install Vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# 8. Copy files
+cp -rf .vimrc ~/.vimrc
+cp -rf .zshrc ~/.zshrc
+
+# initialize Vim plugins
+vim +PluginInstall +qall
+
+# For Plugin YouCorrectMe
+brew install cmake macvim python
+brew install mono go nodejs
+
+cd ~/.vim/bundle/YouCompleteMe
+python3 install.py --all
+
+# If error with vim in Python
+brew install macvim
+alias vim='mvim -v'
+
